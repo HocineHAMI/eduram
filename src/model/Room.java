99015a2c7b7e3d4c45e52c35047027a03574e409
Victor SENE <victor@sene.ovh>
2015-12-12 09:06:24 +0100

    JavaFX Integration

--- src/model/Room.java
+++ src/model/Room.java
@@ -6,5 +6,5 @@ import java.util.ArrayList;
  * Created by victor on 08/12/15.
  */
 public class Room {
-    private ArrayList<Room> neighboorsRooms;
+    private ArrayList<Object> neighboorsRooms;
 }
---
+++ src/view/MapView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by victor on 11/12/15.
+ */
+public class MapView extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
